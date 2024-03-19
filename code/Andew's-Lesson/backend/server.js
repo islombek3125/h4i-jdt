bccb86f0b65905a716748540ccbc2da8cca3c4b6
islombek3125 <ik2236@rit.edu>
2024-03-19 19:19:30 -0400

    twitter clone updated

---
+++ code/Andew's-Lesson/backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const app = express();
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen(port,() => {
+    console.log(`server running on port ${port}`)
+})
