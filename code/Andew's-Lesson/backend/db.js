0bfb8744a9b1dd12f2b3be8979d59e3c33be3d39
islombek3125 <ik2236@rit.edu>
2024-03-19 18:53:47 -0400

    updated Twitter clone

---
+++ code/Andew's-Lesson/backend/db.js
@@ -0,0 +1,7 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    connectionString: "postgresql://postgres:mBTGoCEhUigJjbLRPyWjhjRwMZbhOrJK@roundhouse.proxy.rlwy.net:21823/railway"
+    
+})
+
