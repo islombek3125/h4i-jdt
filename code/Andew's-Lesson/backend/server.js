6c539ea5c8607b13388f7ad562eb5544c79396fd
islombek3125 <ik2236@rit.edu>
2024-03-26 18:35:10 -0400

    updated Twitter clone

--- code/Andew's-Lesson/backend/server.js
+++ code/Andew's-Lesson/backend/server.js
@@ -4,10 +4,16 @@ const cors = require('cors')
 
 
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import the routes from the 'routes.js' module. These are defined routes for the application.
+const routes = require('./routes');
+// Tell the app to use the imported routes for any requests at the root path.
+app.use('/', routes);
+
 
 app.listen(port,() => {
     console.log(`server running on port ${port}`)
